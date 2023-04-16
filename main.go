660ddab8d25692287ce9fa43f833697bed1f2a32
sinulingga23 <sinulinggatwo@gmail.com>
2023-04-16 18:43:14 +0700

    update

--- main.go
+++ main.go
@@ -19,15 +19,12 @@ func main() {
 
 	r.Use(middleware.Logger)
 
-	regsitryPrometheus := prometheus.NewRegistry()
-	regsitryPrometheus.MustRegister(monitoring.TotalRequestApi)
-
 	r.Post("/api/v1/products", api.AddProduct)
 	r.Get("/api/v1/products", api.GetProducts)
 	r.Get("/api/v1/products/{id}", api.GetProduct)
 
-	prompHandler := promhttp.HandlerFor(regsitryPrometheus, promhttp.HandlerOpts{Registry: regsitryPrometheus})
-	r.Handle("/metrics", prompHandler)
+	prometheus.MustRegister(monitoring.TotalRequestApi)
+	r.Handle("/metrics", promhttp.Handler())
 
 	if errListenAndServe := http.ListenAndServe(":8085", r); errListenAndServe != nil {
 		log.Fatalf("Err Listen and Serve: %v", errListenAndServe)
